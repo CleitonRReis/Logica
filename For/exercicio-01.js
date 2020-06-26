545e4c163938b5492a1bdcd9a7f6c692e73218c4
Cleiton Reis <cleitonreis@MacBook-Air-de-Cleiton.local>
2020-06-26 15:56:59 -0300

    Inclui todas as vogais no algoritmo. Se encontrada alguma vogal na palavra/frase ela ficará  maiúscula.

--- For/exercicio-01.js
+++ For/exercicio-01.js
@@ -44,7 +44,7 @@ var palavra = frase.split("");
 console.log(palavra);
 
 for (var i = 0; i < frase.length ; i++){
-    if(frase.charAt(i) == "a"){ 
+    if(frase.charAt(i) == "a" || frase.charAt(i) == "e" || frase.charAt(i) == "i" || frase.charAt(i) == "o" || frase.charAt(i) == "u"){ 
         
         palavra[i] = frase.charAt(i).toUpperCase();
      }
